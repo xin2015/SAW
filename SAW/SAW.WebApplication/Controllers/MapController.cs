9e601751f83dfd78aa0b30db0aa09595eb94938e
LiuHaixin <liuhaixin2016@outlook.com>
2019-01-06 23:46:39 +0800

    map

---
+++ SAW/SAW.WebApplication/Controllers/MapController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SAW.WebApplication.Controllers
+{
+    public class MapController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TianDiTu()
+        {
+            return View();
+        }
+
+        public ActionResult OpenLayers()
+        {
+            return View();
+        }
+
+        public ActionResult BaiDu()
+        {
+            return View();
+        }
+    }
+}
