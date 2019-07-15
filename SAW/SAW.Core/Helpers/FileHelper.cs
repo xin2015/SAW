907d6395c1396eebbe5ec876cff54f2a960ef73b
LiuHaixin <liuhaixin2016@outlook.com>
2019-07-15 11:16:14 +0800

    rename

---
+++ SAW/SAW.Core/Helpers/FileHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAW.Core.Helpers
+{
+    public class FileHelper
+    {
+        public void Rename(string directory, string source, string dest)
+        {
+            string[] files = Directory.GetFiles(directory);
+            foreach (string sourceFile in files)
+            {
+                string destFile = sourceFile.Replace(source, dest);
+                File.Move(sourceFile, destFile);
+            }
+        }
+    }
+}
